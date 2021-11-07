51104ce6c06072d53559a9e7e23a309a8e2a6975
raximvodil <raximvodilrozbek@gmail.com>
2021-11-07 11:54:37 +0500

    7-block 1-video finished

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+
+const application = (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <React.StrictMode>{application}</React.StrictMode>,
+  document.getElementById("root")
 );
